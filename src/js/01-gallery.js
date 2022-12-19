1c729fb4a4b8599463b3cdad4e25d2c14d25acec
Decembric <dmitro15121979@gmail.com>
2022-12-19 12:49:08 +0200

    complete homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+// Change code below this line
+const createGallery = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+};
 console.log(galleryItems);
+const makeElement = galleryItems.map(createGallery).join('');
+const cardsContainer = document.querySelector(".gallery");
+
+cardsContainer.insertAdjacentHTML('beforeend', makeElement);
+cardsContainer.addEventListener('click', onClickImages);
+
+function onClickImages(event) {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== "IMG") {
+    return
+  };
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt', });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,82 @@
+import throttle from "lodash.throttle";
 
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('.feedback-form textarea'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', onTextareaInput);
+
+// localStorage.clear()
+
+
+// function onFormSubmit(event) {
+//   event.preventDefault()
+//   console.log('Get form')
+//   event.currentTarget.reset()
+// }
+
+// function onTextareaInput(event) {
+//   const message = event.currentTarget.value
+//   localStorage.setItem('feedback-form-state', message)
+// }
+
+// function getInfoFromTextarea() {
+//   const savedMessage = localStorage.getItem('feedback-form-state')
+  
+//   if (savedMessage) {
+//     console.log(savedMessage)
+//   }
+// }
+
+const STORAGE_KEY = 'feedback-form-state'
+
+const formData = {}
+
+const textarea = document.querySelector('textarea')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', onFormSubmit)
+textarea.addEventListener('input', throttle(onFormInput, 200))
+form.addEventListener('input', onFormInput)
+
+receiveMessageFromLocalStorage()
+
+function onFormInput(event) {
+  console.log(event.target)
+  formData[event.target.name] = event.target.value
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+// function onTextareaInput(event) {
+//   localStorage.setItem(STORAGE_KEY, message)
+  
+// }
+
+function onFormSubmit(event) {
+  event.preventDefault()
+  if (message) {
+    event.target.reset() 
+    localStorage.removeItem(STORAGE_KEY)
+    const message = event.target.value
+    // const name = event.target.name
+    console.log(message)
+    // console.log(name)
+    localStorage.setItem(STORAGE_KEY, message)
+    
+  }
+}
+
+function receiveMessageFromLocalStorage() {
+  let savedMessage = localStorage.getItem(STORAGE_KEY)
+  if (savedMessage) {
+    savedMessage = JSON.parse(savedMessage)
+    console.log(savedMessage)
+    Object.entries(savedMessage).forEach(([name, value]) => {
+      console.log(name, value)
+      form.elements[name].value = value
+      formData[name] = value
+    })
+  }
+}

74539ed1255d3baad227b2906dda41f8246d2301
Decembric <dmitro15121979@gmail.com>
2022-12-20 12:39:34 +0200

    Fixed homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const storage = {}
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
     
+
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
 
-player.on('timeupdate', throttle(onPlay, 1000));
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds)
-}
 
+if (storage !== null) {
+  player.on('timeupdate', throttle(onPlay, 1000));
+  function onPlay({ seconds }) {
+   storage = localStorage.setItem('videoplayer-current-time', seconds)
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,46 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
-
 const textarea = document.querySelector('textarea');
+const input = document.querySelector('input')
 const form = document.querySelector('form');
 
-form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', throttle(onFormInput, 200));
-form.addEventListener('input', onFormInput);
+if (textarea.value !== "" || input.name !== "") {
 
-receiveMessageFromLocalStorage();
+  form.addEventListener('submit', onFormSubmit);
+  textarea.addEventListener('input', throttle(onFormInput, 200));
+  form.addEventListener('input', onFormInput);
 
-function onFormInput(event) {
-  console.log(event.target);
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
+  receiveMessageFromLocalStorage();
 
-
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.target.reset(); 
-  localStorage.removeItem(STORAGE_KEY);
-  const message = event.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
-function receiveMessageFromLocalStorage() {
-  let savedMessage = localStorage.getItem(STORAGE_KEY);
-  if (savedMessage) {
-    savedMessage = JSON.parse(savedMessage);
-    console.log(savedMessage);
-    Object.entries(savedMessage).forEach(([name, value]) => {
-      console.log(name, value);
-      form.elements[name].value = value;
-      formData[name] = value;
-    })
+  function onFormInput(event) {
+    let message = localStorage.getItem(STORAGE_KEY);
+    if (message) {
+      message = JSON.parse(message);
+    } else {
+      message = {}
+    }
+    message[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(message));
   }
+
+
+  function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
+  function receiveMessageFromLocalStorage() {
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+      savedMessage = JSON.parse(savedMessage);
+      Object.entries(savedMessage).forEach(([name, value]) => {
+        form.elements[name].value = value;
+        savedMessage = '';
+      })
+    }
+  }
+} else {
+  console.log('Input full data')
 }
